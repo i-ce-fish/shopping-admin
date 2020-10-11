a243be9d7f4a786e1d9caa20e93ffeedf3099b0b
binyu <876644846@qq.com>
2020-10-11 12:45:41 +0800

    INIT

---
+++ src/views/test/demo/father.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>这个是父组件
+    <div>调用子组件</div>
+    <user-input width="400px"></user-input>
+
+  </div>
+</template>
+<script>
+import input from '@/views/test/demo/input'
+
+export default {
+  components: { userInput: input },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.father {
+}
+</style>
---
+++ src/views/test/demo/input.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-input :style="{width:width}">
+    </el-input>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  props: {
+    width: {
+      type: String,
+      default: '100px',
+      required: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+}
+</style>
