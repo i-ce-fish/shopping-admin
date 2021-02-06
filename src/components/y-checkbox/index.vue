2b0fc8b493498d9166fa820d48b8adfda8245137
libiny <libiny@minstone.com.cn>
2021-02-06 14:37:55 +0800

    优化checkbox

--- src/components/y-checkbox/index.vue
+++ src/components/y-checkbox/index.vue
@@ -1,22 +1,17 @@
 <template>
-  <y-tooltip :tips="tips">
-
     <el-checkbox-group
-        v-model="result"
+        v-model="model"
         v-on="$listeners"
         v-bind="$attrs"
     >
       <el-checkbox
-
-          v-for="item of options"
-          :id="item.id"
-          :key="item.value"
+          v-for="(item,index) of options"
           v-bind="item"
+          :key="index"
       >
-        {{item.label}}
+        {{ item.label }}
       </el-checkbox>
     </el-checkbox-group>
-  </y-tooltip>
 </template>
 <script>
 export default {
@@ -28,44 +23,31 @@ export default {
     options: {
       type: [Array, Object],
       default: () => [{
-        value: '选项1',
-        label: '黄金糕'
+        value: 1,
+        label: '是'
       }, {
-        value: '选项2',
-        label: '双皮奶'
-      }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
+        value: 0,
+        label: '否'
       }]
     },
-    // 输入提示
-    tips: {
-      type: String,
-      default: ''
-    }
+    // key:{type:String},
+    // label:{type:String}
   },
   created() {
   },
   data() {
-    return { result: this.value }
+    return { model: this.value }
   },
-  watch: {
-    value(val) {
-      this.result = val
-    }
-  },
+  //todo 删了会怎样
+  // watch: {
+  //   value(val) {
+  //     this.model = val
+  //   }
+  // },
   methods: {
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.ycheckbox {
-}
 </style>
